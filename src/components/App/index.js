ef5b8a05452907b852194d1b4293e0451aa6d2ac
NeoCortexFr <sebastien.latouche.28@gmail.com>
2021-08-07 21:57:19 +0200

    Arborescence générale + add React Router Dom

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import reactLogo from './react-logo.svg'; <img src={reactLogo} alt="react logo" />
+import './styles.css';
+
+const App = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+export default App;
