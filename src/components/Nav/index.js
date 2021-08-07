ce7827fda4e60aeb9ad1d85254c430d4d68bb537
NeoCortexFr <sebastien.latouche.28@gmail.com>
2021-08-07 22:46:42 +0200

    nav 1st step ok

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './nav.scss';
+
+const Nav = () => (
+  <nav className="nav">
+    <ul className="nav_list">
+      <li>Système solaire</li>
+      <li>Sondes spatiales</li>
+      <li>Relativité restreinte</li>
+      <li>Relativité générale</li>
+      <li>Physique / Astrophysqiue</li>
+      <li>Livres / Ouvrages</li>
+      <li>Glossaire</li>
+    </ul>
+  </nav>
+);
+export default Nav;
